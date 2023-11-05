9fde0e0ef67cf5b3b89ad5ce85dbb88ef011fd1b
il0naa <136170246+il0naa@users.noreply.github.com>
2023-11-05 22:12:13 +0100

    gallery 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
-// Add imports above this line
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 import { galleryItems } from './gallery-items';
-// Change code below this line
+
+const list = document.querySelector('.gallery');
+
+function renderList(images) {
+    const markup = images.map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+              <img class="gallery__image" src="${preview}" alt="${description}">
+          </a>
+      </li>`;
+    }).join('');
+  
+    list.innerHTML = markup;
+  };
+
+  renderList(galleryItems);
+
+  list.style.listStyle = 'none';
+
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionDelay: 250,
+  });
 
 console.log(galleryItems);
