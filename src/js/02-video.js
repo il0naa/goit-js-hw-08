56a86de03b270f2e57f26c5ab753f2dc13e638fe
il0naa <136170246+il0naa@users.noreply.github.com>
2023-11-05 22:50:41 +0100

    video task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
 
+const iframe = document.getElementById('vimeo-player');
+const player = new Vimeo.Player(iframe);
+
+player.on('timeupdate', function(data) {
+  const currentTime = data.seconds;
+  localStorage.setItem('videoplayer-current-time', currentTime);
+});
+
+window.onload = function() {
+  const currentTime = localStorage.getItem('videoplayer-current-time');
+  if (currentTime) {
+    player.setCurrentTime(parseFloat(currentTime));
+  }
+};
