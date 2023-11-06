3e2f8cbb0402ff4e522c0d1776e0aa2b33cf1009
il0naa <136170246+il0naa@users.noreply.github.com>
2023-11-06 10:16:30 +0100

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import throttle from 'lodash.throttle';
 
+const feedbackForm = document.querySelector('.feedback-form');
+const emailInput = document.querySelector('input[name="email"]');
+const messageInput = document.querySelector('textarea[name="message"]');
+
+function saveFormData() {
+  const formData = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+function restoreForm() {
+  const formData = JSON.parse(localStorage.getItem('feedback-form-state'));
+  if (formData) {
+    emailInput.value = formData.email || '';
+    messageInput.value = formData.message || '';
+  }
+}
+
+const throttledSaveForm = throttle(saveFormData, 500);
+
+emailInput.addEventListener('input', throttledSaveForm);
+messageInput.addEventListener('input', throttledSaveForm);
+
+window.onload = () => {
+  restoreForm();
+};
+
+feedbackForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+  localStorage.removeItem('feedback-form-state');
+
+  const formData = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  console.log(formData);
+});
